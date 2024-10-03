c69baf11b3b6c8d496033f0f474acb3937c5377f
Jack Mahony <jackmahonyy@gmail.com>
2024-10-03 18:32:08 +1000

    Fixed security issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
     # Only perform the API request if a user_id is present
     if params[:user_id].present?
       user_id = params[:user_id]
-      @response = HTTParty.get("https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=676491a626284d418ddf92d11eb07d3f&user_id=#{user_id}&format=json&nojsoncallback=1")
+      api_key = Rails.application.credentials.dig(:flickr, :api_key)
+
+      @response = HTTParty.get("https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=#{api_key}&user_id=#{user_id}&format=json&nojsoncallback=1")
       @photos = JSON.parse(@response.body)
 
       # Check if the API returned an error
